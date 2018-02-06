63c63906fc4174926aa454b1dce661c9876b39c6
hansexe <hansexebio@upeu.edu.pe>
2018-02-06 15:46:55 -0500

    avance de asistencia

--- resources/views/sesiones/asistencia/show.blade.php
+++ resources/views/sesiones/asistencia/show.blade.php
@@ -10,7 +10,7 @@
                                
    <div class="row justify-content: center">
    <div class="col-lg-6 col-md-6 col-md-offset-2">
-		<h3>Asistencias</h3>
+		<h3>Asistencias <i class="ti-clipboard"></i></h3>
 	   
 	</div>
    </div>
@@ -22,48 +22,69 @@
 					 
                     <th>Familia</th>
  	<th>Asistencia de {{ $fechas->Fecha}}</th> 
-
+<th>Opciones</th>
 				</thead>
  
  
 @foreach ($auditoria as $au)
+ <?php $bandera = 0 ?>
+ <?php $nulo = 0 ?>
 				<tr>
 					<td>{{ $au->Nom_fam}}</td>
-						
+	
+
 @foreach($asistencia as $asis)
-                     
-<?php if ($au->idFamilia == $asis->Familia_idFamilia): ?>
 
-<?php if ($asis->Estado_asistencia== '1'): ?>
-     <td><?php echo 'asistio' ?></td>  
+@if($au->idFamilia == $asis->Familia_idFamilia && $asis->Estado_asistencia == 1) <!-- aqui igualo la id -->
+ <?php $bandera++ ?>
+ @endif
 
-@break
-<?php elseif($asis->Estado_asistencia== '0'): ?>
-    <td><?php echo 'no asistio' ?></td>
-@break
+ @if($au->idFamilia == $asis->Familia_idFamilia && $asis->Estado_asistencia == 2) <!-- aqui igualo la id -->
+ <?php $nulo++ ?>
+ @endif
+ @endforeach
 
-<?php elseif($asis->Estado_asistencia== null): ?>
+@if ($bandera)
+<td><label class="box-label">
+        <input type="checkbox" class="box-check" checked disabled/>
+        <span class="box-text"></span>
+    </label>  
+</td> 
 
-  <td><?php echo 'no registrado' ?></td>
-@break
+<td><a data-target="#modal-EditAsis-" data-toggle="modal"><button class="btn"><i class="ti-pencil-alt2"></i> Editar</button></a></td>
 
-<?php endif ?>
+@elseif($nulo)
+<td><label class="box-label">
+        <input type="checkbox" class="box-check" disabled/>
+        <span class="box-text"></span>
+    </label></td> 
 
-<?php endif ?>
-@endforeach
+<td><a data-target="#modal-ReRegAsis-" data-toggle="modal"><button class="btn btn-info"><i class="ti-receipt"></i> Registrar E.</button></a></td>
+@else
+<td><label class="box-label">
+        <input type="checkbox" class="box-check" disabled/>
+        <span class="box-text"></span>
+    </label></td>
+
+<td><a data-target="#modal-RegAsis-" data-toggle="modal"><button class="btn btn-info"><i class="ti-receipt"></i> Registrar</button></a></td>
+
+@endif
+
+
 
 
 </tr>
 				@endforeach
                                 </table>
 
-                            </div>
-                            {{$auditoria->render()}}
+                            </div> 
+                           {{$auditoria->render()}}
                         </div>
                     </div>
 
-
-
+  @include('sesiones.asistencia.ReRegAsis') 
+  @include('sesiones.asistencia.RegAsis') 
+  @include('sesiones.asistencia.EditAsis') 
                 </div>
             </div>
 
