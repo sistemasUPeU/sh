bc8b252fe9f570aa2e7f53c24d0e0c446ba22433
hansexe <hansexebio@upeu.edu.pe>
2018-02-21 15:13:44 -0500

    css card

--- resources/views/reporte/asistencia/index.blade.php
+++ resources/views/reporte/asistencia/index.blade.php
@@ -18,12 +18,37 @@
  
                    
                             <div class="content table-responsive table-full-width">
-                                <table class="table table-striped">
+                             
                                     <thead>
 					 
                     <th>Familia</th>
  	<th>Reporte de asistencia</th>
 				</thead>
- 
- 
+ </div>
+</div>
+</div></div></div>
+
+<div class="row">
+  
+  <article class="col-md-12">
+                <div class="row">
+                    <div class="col-md-4">
+                        
+                        <div class="card">
+                            <div class="table table-info">
+                                <h5 class="category-social">
+                        <i class="fa fa-twitter"></i> Twitter
+                      </h5>
+                                <h4 class="card-caption">
+                        <a href="#">"Esa mentira solapada trajo a un tú completamente diferente de lo que había imaginado"</a>
+                      </h4>
+                                
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    </article>
+</div>
+
+
 @endsection
