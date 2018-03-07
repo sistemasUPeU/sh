9bb5ed971d7ffe25ef1ffbe2c26897a9ef4abd5e
zannier7 <zanniervargas@upeu.edu.pe>
2018-03-07 11:44:43 -0500

    error principal corregido

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -369,6 +369,17 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     $('.carousel').carousel({
         interval: 3000
     })
+    $(function() {
+
+        var $body = $(document);
+        $body.bind('scroll', function() {
+            // "Disable" the horizontal scroll.
+            if ($body.scrollLeft() !== 0) {
+                $body.scrollLeft(0);
+            }
+        });
+
+    });
 </script>
 </body>
 </html>
